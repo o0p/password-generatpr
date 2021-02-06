f5e78590d192dea6a6027abd1db3a8cff91c0dd3
Russell <russelldev@protonmail.com>
2021-02-06 13:37:10 +0000

    Add ability to copy stuff to clipboard

--- script.js
+++ script.js
@@ -16,6 +16,19 @@ generateEl.addEventListener('click', () => {
     resultEl.innerText = generatePassword(hasLower, hasUpper, hasNumber, hasSymbol, length);
 });
 
+clipboardEl.addEventListener('click', () => {
+    const textarea = document.createElement('textarea');
+    const password = resultEl.innerText;
+    
+    if (!password) {return};
+    textarea.value = password;
+    document.body.appendChild(textarea);
+    textarea.select();
+    document.execCommand('copy');
+    textarea.remove();
+    alert('Password coppied to clipboard!');
+});
+
 function generatePassword(lower, upper, number, symbol, length) {
     let generatedPassword = '';
     const typesCount = lower + upper + number + symbol;
